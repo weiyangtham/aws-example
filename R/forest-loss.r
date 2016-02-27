dcf4a6c500efd1ad2106d5497b4d505f34a78d7d
Matt Strimas-Mackey <mstrimas@gmail.com>
2016-02-27 12:45:56 -0800

    forest loss example

---
+++ R/forest-loss.r
@@ -0,0 +1,27 @@
+library(readr)
+library(dplyr)
+library(ggplot2)
+
+fra <- read_csv("data/fao-fra.csv", col_types = "ccidddd")
+
+# summarize by continent
+fra_region <- fra %>% 
+  group_by(country) %>% 
+  filter(sum(!is.na(forest)) == 5, sum(!is.na(nat_for)) == 5) %>% 
+  group_by(region, year) %>% 
+  summarize_each(funs(sum), land_area, forest, nat_for) %>% 
+  group_by(region) %>% 
+  # annual forest loss
+  mutate(change = 100 * (forest / lag(forest) - 1) / (year - lag(year)),
+         nat_change = 100 * (nat_for / lag(nat_for) - 1) / (year - lag(year))) %>% 
+  ungroup %>% 
+  mutate(region = reorder(factor(region), -change, FUN = max, na.rm = T))
+
+# plot natural forest loss
+fra_region %>% 
+  filter(year != 1990) %>% 
+  ggplot(aes(x = year, y = nat_change, color = region)) +
+  geom_point() +
+  geom_line() + 
+  labs(x = "Year", y = "% change in natural forest cover / year") +
+  scale_color_brewer(name = "Continent", palette = "Set1")
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# aws-example
-An example repository for a tutorial on using RStudio Server on Amazon AWS
+# RStudio in the Cloud with AWS
+
+This is an example repository for a series of tutorials on using RStudio Server on Amazon Web Services (AWS). To tutorials are available at [strimas.com](http://strimas.com).
