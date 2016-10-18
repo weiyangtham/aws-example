103b3beb106809afc9bf3e4cc0e9b82aa02e2037
Matt Strimas-Mackey <mstrimas@gmail.com>
2016-10-18 11:03:47 -0400

    modified to accomodate updated blog post

--- R/forest-loss.r
+++ R/forest-loss.r
@@ -1,6 +1,4 @@
-library(readr)
-library(dplyr)
-library(ggplot2)
+library(tidyverse)
 
 fra <- read_csv("data/fao-fra.csv", col_types = "ccidddd")
 
@@ -16,7 +14,7 @@ fra_region <- fra %>%
          nat_change = 100 * (nat_for / lag(nat_for) - 1) / (year - lag(year))) %>% 
   ungroup %>% 
   mutate(region = reorder(factor(region), -change, FUN = max, na.rm = T))
-write_csv(fra_region, "data/fao-fra-region.csv")
+write_csv(fra_region, "output/fao-fra-region.csv")
 
 # plot natural forest loss
 fra_region %>% 
@@ -26,4 +24,4 @@ fra_region %>%
   geom_line() + 
   labs(x = "Year", y = "% change in natural forest cover / year") +
   scale_color_brewer(name = "Continent", palette = "Set1")
-ggsave('data/forest-change.png')
+ggsave('output/forest-change.png')
